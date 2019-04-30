d3ea2827db55d80099dc2c775027be37118897e9
Marcin Modestowicz <marcin@modestowicz.com>
2019-04-30 07:32:47 +0200

    Update App

--- app.js
+++ app.js
@@ -1,40 +1,82 @@
-import './components/list/list.js';
+import './components/sortableTable/sortableTable.js';
 import './components/searchInput/searchInput.js';
 import './components/sortableHeader/sortableHeader.js';
 
-const listElement = document.querySelector('sortable-list');
-const searchInputElement = document.querySelector('search-input')
-const columns = [
-    {
-        key: 'mass',
-        value: 'Mass'
-    },
-    {
-        key: 'name',
-        value: 'Description'
-    },
-    {
-        key: 'recclass',
-        value: 'Classification'
-    },
-    {
-        key: 'year',
-        value: 'Date'
-    }
-];
+class App {
+    sortBy = null;
+    sortDirection = null;
+    search = null;
+    columns = [
+        {
+            key: 'mass',
+            value: 'Mass'
+        },
+        {
+            key: 'name',
+            value: 'Name'
+        },
+        {
+            key: 'class',
+            value: 'Class'
+        },
+        {
+            key: 'year',
+            value: 'Year'
+        }
+    ];
 
-listElement.columns = columns;
+    constructor() {
+        this.sortableTableElement = document.querySelector('sortable-table');
+        this.searchInputElement = document.querySelector('search-input')
 
-searchInputElement.addEventListener('onChange', ({ detail }) => {
-    fetch(`http://5cc47e573f761f001422d5e9.mockapi.io/api/v1/meteorites?sortBy=recclass&search=${detail}`)
-        .then(response => response.json())
-        .then(json => {
-            listElement.items = json;
+        this.searchInputElement.addEventListener('searchValueChange', ({ detail }) => {
+            this.search = detail.trim();
+
+            this._getData();
         });
-});
+        
+        this.sortableTableElement.addEventListener('tableSortChange', ({ detail: { direction, key } }) => {
+            this.sortBy = key;
+            this.sortDirection = direction;
 
-fetch('http://5cc47e573f761f001422d5e9.mockapi.io/api/v1/meteorites?sortBy=recclass&search=')
-    .then(response => response.json())
-    .then(json => {
-        listElement.items = json;
-    });
+            this._getData();
+        });
+
+        this.sortableTableElement.columns = this.columns;
+
+        this._getData();
+    }
+
+    _transformResponse(item) {
+        const {
+            mass,
+            name,
+            recclass,
+            year
+        } = item;
+
+        return {
+            mass,
+            name,
+            class: recclass,
+            year: (new Date(year)).getFullYear()
+        };
+    }
+
+    _getData() {
+        const sortBy = this.sortBy && this.sortDirection
+            ? `sortBy=${this.sortBy}&order=${this.sortDirection}`
+            : '';
+        const search = this.search
+            ? `${sortBy ? '&' : ''}search=${this.search}`
+            : '';
+
+        fetch(`http://5cc47e573f761f001422d5e9.mockapi.io/api/v1/meteorites?${sortBy}${search}`)
+            .then(response => response.json())
+            .then(json => {
+                this.sortableTableElement.items = json.map(this._transformResponse);
+            });
+    }
+}
+
+export default new App();
